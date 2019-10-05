8013c9107a47b9fd047d278bbf3952c0d1e052f7
shashaankdubey <shashaankdubey444@gmail.com>
2019-10-05 22:03:28 +0530

    admin python

---
+++ sastakart_project/admin.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns,include, url
+from django.contrib import admin
+from sastakart.views import *
+
+
+# admin.autodiscover()
+urlpatterns = patterns('',    
+    url(r'^', include('django.contrib.auth.urls')),
+    url(r'^', include('sastakart.urls',namespace='sastakart')),
+    url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
+    url(r'^admin/', include(admin.site.urls)),    
+) 
